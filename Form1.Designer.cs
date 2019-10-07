8fd8e1a44594317ecb8e64cc94feafadc1382c12
Jin <form_china@163.com>
2019-10-07 19:59:22 +0800

    first commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,168 @@
+﻿namespace FileAdmin
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button3 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.button4 = new System.Windows.Forms.Button();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(170, 35);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "已保存";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(4, 34);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(79, 23);
+            this.button2.TabIndex = 0;
+            this.button2.Text = "刷新配置";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(10, 18);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(77, 12);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "向此拖入文件";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.ColumnHeadersHeight = 25;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.DisableResizing;
+            this.dataGridView1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dataGridView1.Location = new System.Drawing.Point(3, 3);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 50;
+            this.dataGridView1.RowTemplate.Height = 40;
+            this.dataGridView1.Size = new System.Drawing.Size(411, 254);
+            this.dataGridView1.TabIndex = 6;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            this.dataGridView1.CellValueChanged += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellValueChanged);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(89, 35);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 7;
+            this.button4.Text = "删除";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Location = new System.Drawing.Point(12, 64);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(425, 286);
+            this.tabControl1.TabIndex = 8;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.dataGridView1);
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(417, 260);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "tabPage1";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(192, 74);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "tabPage2";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AllowDrop = true;
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Inherit;
+            this.ClientSize = new System.Drawing.Size(449, 362);
+            this.Controls.Add(this.tabControl1);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button3);
+            this.MinimumSize = new System.Drawing.Size(350, 400);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.SizeChanged += new System.EventHandler(this.Form1_SizeChanged);
+            this.DragEnter += new System.Windows.Forms.DragEventHandler(this.Form1_DragEnter);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage tabPage2;
+
+
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,309 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+namespace FileAdmin
+{
+    public partial class Form1 : Form
+    {
+        List<string> filePathList = new List<string>();
+        List<string> fileNameList = new List<string>();
+        Image image;
+        DataTable dt = new DataTable();
+        int gap = 40;
+        string configePath = Application.StartupPath + "//data.data";
+        DataTable mydt = new DataTable("myTableName");
+        string filePath, fileName;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.AllowDrop = true;
+            readConfige();
+            sizeChange();
+        }
+
+        private void Form1_DragEnter(object sender, DragEventArgs e)
+        {
+            label1.Left = 0;
+            //获取拖放数据
+            if (e.Data.GetDataPresent(DataFormats.FileDrop))
+            {
+                string[] content = (string[])e.Data.GetData(DataFormats.FileDrop);
+                for (int i = 0; i < content.Length; i++)
+                {
+                    //这是全路径
+                   filePath = content[i];
+
+                   label1.Text = filePath;
+                   fileName = System.IO.Path.GetFileName(filePath);
+                   if (Directory.Exists(filePath))
+                   {
+                       image = SystemIcon.GetDirectoryIcon(filePath, true).ToBitmap();
+                   }
+                   else
+                   {
+                       image = SystemIcon.GetIcon(filePath, true).ToBitmap();
+                   }
+                   addList();
+                }
+            }
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+        void sizeChange()
+        {
+            label1.Left = (this.Width - label1.Width) / 2;
+
+            tabControl1.Left = gap;
+            tabControl1.Width = this.Width - 2 * gap;
+            tabControl1.Height = this.Height - tabControl1.Top - gap;
+        }
+        private void Form1_SizeChanged(object sender, EventArgs e)
+        {
+            sizeChange();
+        }
+        void addList()
+        {
+            
+            dt.Rows.Add(image ,fileName, filePath);
+            
+            File.AppendAllText(configePath, fileName+"|"+ filePath+"\r\n");
+            
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+            if (label1.Text != "向此拖入文件")
+            {
+                //取文件名
+                addList();
+
+
+            }
+        }
+        void readConfige()
+        {
+            if (!File.Exists(configePath))
+            {
+                FileStream fs;
+                fs = File.Create(configePath);
+                fs.Close();
+            }
+            
+            dt = new DataTable();
+            System.IO.StreamReader mysr = new System.IO.StreamReader(configePath);
+            string strline;
+
+            filePathList = new List<string>();
+            fileNameList = new List<string>();
+            string[] aryline;
+            dt.Columns.Add("  ");
+            dt.Columns[0].DataType = typeof(Bitmap);
+            
+            dt.Columns.Add("文件名");
+            dt.Columns.Add("路径");
+            while ((strline = mysr.ReadLine()) != null)
+            {
+                aryline = strline.Split(new char[] { '|' });
+                if (Directory.Exists(aryline[1]))
+                {
+                    image = SystemIcon.GetDirectoryIcon(aryline[1], true).ToBitmap();
+                }
+                else
+                {
+                    image = SystemIcon.GetIcon(aryline[1], true).ToBitmap();
+                }
+                dt.Rows.Add(image,aryline[0],aryline[1]);
+                
+            }
+            mysr.Close();
+            dataGridView1.DataSource = dt;
+            dataGridView1.Columns[0].Resizable = DataGridViewTriState.False;
+
+            dataGridView1.Columns[0].Width = 10;
+            dataGridView1.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
+        }
+        void writeConfige(string str)
+        {
+            try
+            {
+                
+            File.WriteAllText(configePath, str);
+            button3.Text = "已保存";
+            }
+            catch (Exception)
+            {
+                
+            }
+
+ 
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            readConfige();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            string str="";
+            int index = 0;
+            for (int i = 0; i < dt.Rows.Count; i++)
+            {
+                str += dt.Rows[i][1] +"|"+ dt.Rows[i][2]+ "\r\n";
+            }
+            foreach (var item in dt.Columns)
+            {
+
+                index++;
+            }
+            writeConfige(str);
+        }
+
+        private void tableLayoutPanel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+        DataTable deleteData;
+        private void button4_Click(object sender, EventArgs e)
+        {
+                deleteData = (DataTable)dataGridView1.DataSource;
+                for (int j = 0; j < this.dataGridView1.Rows.Count; j++)
+                {
+                    if (this.dataGridView1.SelectedRows.Count > 0)
+                    {
+                        DataRowView drv = this.dataGridView1.SelectedRows[0].DataBoundItem as DataRowView;
+                        try
+                        {
+
+                            drv.Delete();
+                            button3.Text = "未保存";
+                        }
+                        catch (Exception)
+                        {
+
+                        }
+                    }
+
+            }
+            
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void dataGridView1_CellValueChanged(object sender, DataGridViewCellEventArgs e)
+        {
+            button3.Text = "未保存";
+        }
+
+
+    }
+    class SystemIcon
+    {
+        [StructLayout(LayoutKind.Sequential, CharSet = CharSet.Auto)]
+        public struct SHFILEINFO
+        {
+            public IntPtr hIcon;
+            public int iIcon;
+            public uint dwAttributes;
+            [MarshalAs(UnmanagedType.ByValTStr, SizeConst = 260)]
+            public string szDisplayName;
+            [MarshalAs(UnmanagedType.ByValTStr, SizeConst = 80)]
+            public string szTypeName;
+        }
+        [DllImport("Shell32.dll", EntryPoint = "SHGetFileInfo", SetLastError = true, CharSet = CharSet.Auto)]
+        public static extern IntPtr SHGetFileInfo(string pszPath, uint dwFileAttributes, ref SHFILEINFO psfi, uint cbFileInfo, uint uFlags);
+        [DllImport("User32.dll", EntryPoint = "DestroyIcon")]
+        public static extern int DestroyIcon(IntPtr hIcon);
+        #region API 参数的常量定义
+        public enum FileInfoFlags : uint
+        {
+            SHGFI_ICON = 0x000000100, // get icon
+            SHGFI_DISPLAYNAME = 0x000000200, // get display name
+            SHGFI_TYPENAME = 0x000000400, // get type name
+            SHGFI_ATTRIBUTES = 0x000000800, // get attributes
+            SHGFI_ICONLOCATION = 0x000001000, // get icon location
+            SHGFI_EXETYPE = 0x000002000, // return exe type
+            SHGFI_SYSICONINDEX = 0x000004000, // get system icon index
+            SHGFI_LINKOVERLAY = 0x000008000, // put a link overlay on icon
+            SHGFI_SELECTED = 0x000010000, // show icon in selected state
+            SHGFI_ATTR_SPECIFIED = 0x000020000, // get only specified attributes
+            SHGFI_LARGEICON = 0x000000000, // get large icon
+            SHGFI_SMALLICON = 0x000000001, // get small icon
+            SHGFI_OPENICON = 0x000000002, // get open icon
+            SHGFI_SHELLICONSIZE = 0x000000004, // get shell size icon
+            SHGFI_PIDL = 0x000000008, // pszPath is a pidl
+            SHGFI_USEFILEATTRIBUTES = 0x000000010, // use passed dwFileAttribute
+            SHGFI_ADDOVERLAYS = 0x000000020, // apply the appropriate overlays
+            SHGFI_OVERLAYINDEX = 0x000000040 // Get the index of the overlay
+        }
+        public enum FileAttributeFlags : uint
+        {
+            FILE_ATTRIBUTE_READONLY = 0x00000001,
+            FILE_ATTRIBUTE_HIDDEN = 0x00000002,
+            FILE_ATTRIBUTE_SYSTEM = 0x00000004,
+            FILE_ATTRIBUTE_DIRECTORY = 0x00000010,
+            FILE_ATTRIBUTE_ARCHIVE = 0x00000020,
+            FILE_ATTRIBUTE_DEVICE = 0x00000040,
+            FILE_ATTRIBUTE_NORMAL = 0x00000080,
+            FILE_ATTRIBUTE_TEMPORARY = 0x00000100,
+            FILE_ATTRIBUTE_SPARSE_FILE = 0x00000200,
+            FILE_ATTRIBUTE_REPARSE_POINT = 0x00000400,
+            FILE_ATTRIBUTE_COMPRESSED = 0x00000800,
+            FILE_ATTRIBUTE_OFFLINE = 0x00001000,
+            FILE_ATTRIBUTE_NOT_CONTENT_INDEXED = 0x00002000,
+            FILE_ATTRIBUTE_ENCRYPTED = 0x00004000
+        }
+        #endregion
+        /// <summary>
+        /// 获取文件类型的关联图标
+        /// </summary>
+        /// <param name="fileName">文件类型的扩展名或文件的绝对路径</param>
+        /// <param name="isLargeIcon">是否返回大图标</param>
+        /// <returns>获取到的图标</returns>
+        public static Icon GetIcon(string fileName, bool isLargeIcon)
+        {
+            SHFILEINFO shfi = new SHFILEINFO();
+            IntPtr hI;
+            if (isLargeIcon)
+                hI = SHGetFileInfo(fileName, 0, ref shfi, (uint)Marshal.SizeOf(shfi), (uint)FileInfoFlags.SHGFI_ICON | (uint)FileInfoFlags.SHGFI_USEFILEATTRIBUTES | (uint)FileInfoFlags.SHGFI_LARGEICON);
+            else
+                hI = SHGetFileInfo(fileName, 0, ref shfi, (uint)Marshal.SizeOf(shfi), (uint)FileInfoFlags.SHGFI_ICON | (uint)FileInfoFlags.SHGFI_USEFILEATTRIBUTES | (uint)FileInfoFlags.SHGFI_SMALLICON);
+            Icon icon = Icon.FromHandle(shfi.hIcon).Clone() as Icon;
+            DestroyIcon(shfi.hIcon); //释放资源
+            return icon;
+        }
+        /// <summary> 
+        /// 获取文件夹图标
+        /// </summary> 
+        /// <returns>图标</returns> 
+        public static Icon GetDirectoryIcon(string filePath, bool isLargeIcon)
+        {
+            SHFILEINFO _SHFILEINFO = new SHFILEINFO();
+            IntPtr _IconIntPtr;
+            if (isLargeIcon)
+            {
+                _IconIntPtr = SHGetFileInfo(filePath, 0, ref _SHFILEINFO, (uint)Marshal.SizeOf(_SHFILEINFO), ((uint)FileInfoFlags.SHGFI_ICON | (uint)FileInfoFlags.SHGFI_LARGEICON));
+            }
+            else
+            {
+                _IconIntPtr = SHGetFileInfo(filePath, 0, ref _SHFILEINFO, (uint)Marshal.SizeOf(_SHFILEINFO), ((uint)FileInfoFlags.SHGFI_ICON | (uint)FileInfoFlags.SHGFI_SMALLICON));
+            }
+            if (_IconIntPtr.Equals(IntPtr.Zero)) return null;
+            Icon _Icon = System.Drawing.Icon.FromHandle(_SHFILEINFO.hIcon);
+            return _Icon;
+        }
+    }
+}
